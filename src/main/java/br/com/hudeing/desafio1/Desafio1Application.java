91459e51b9f1d4dfd6150de8477d745bd7f6a0af
Aller Augusto de Souza <allerasouza@gmail.com>
2024-01-14 11:25:56 -0300

    feat: add output command line implementation

--- src/main/java/br/com/hudeing/desafio1/Desafio1Application.java
+++ src/main/java/br/com/hudeing/desafio1/Desafio1Application.java
@@ -1,11 +1,19 @@
 package br.com.hudeing.desafio1;
 
+import br.com.hudeing.desafio1.entities.Order;
+import br.com.hudeing.desafio1.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class Desafio1Application implements CommandLineRunner {
+
+	@Autowired
+	private OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Desafio1Application.class, args);
@@ -13,6 +21,11 @@ public class Desafio1Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("CommandLineRunner");
+		Locale.setDefault(Locale.US); // Setar US para poder imprimir com ponto
+		Order order = new Order(1034, 150.00, 20.0);
+//		Order order = new Order(2282, 800.00, 10.0);
+//		Order order = new Order(1309, 95.90, 0.0);
+		System.out.println("Pedido código " + order.getCode());
+		System.out.printf("Valor total: R$ %.2f%n", orderService.total(order));
 	}
 }
